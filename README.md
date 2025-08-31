953c87edc1439aa8cbc681c5fbb8bd04e9e31bd4
thaddavis <thaddadavis@gmail.com>
2025-08-31 16:48:46 -0400

    boilerplate code

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # TLDR
 
 Cloud Function for ingesting knowledge into a Pinecone Vector DB
+
+## How to run this project
+
+- npm run watch
+- npm run compile
+- npm run start
+
+## How to deploy to GCF
+
+gcloud functions deploy processPubSubMessage
+--runtime=nodejs22
+--trigger-topic=csv-processing-topic
+--region=us-east1
+--memory=1GB
+--timeout=540s
+--source ./build
+--entry-point=processPubSubMessage
+--gen2
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+// Pub/Sub-triggered function
+const fs = require("fs");
+const path = require("path");
+
+// @ts-ignore
+exports.processPubSubMessage = async (reqOrEvent, contextOrRes) => {
+  let message = "{}";
+  let attributes = {};
+  
+  try {
+
+    // Detect if triggered via HTTP or Pub/Sub
+    if (reqOrEvent.body) { // HTTP-triggered (local testing with curl)
+      console.log("Detected HTTP trigger for local testing.");
+      const pubSubMessage = typeof reqOrEvent.body === "string" ? JSON.parse(reqOrEvent.body) : reqOrEvent.body; // Handle req.body format
+      console.log("pubSubMessage", pubSubMessage);
+      message = pubSubMessage.data
+        ? Buffer.from(pubSubMessage.data, "base64").toString()
+        : "{}";
+      attributes = pubSubMessage.attributes || {};
+
+      // Send HTTP response
+      contextOrRes.status(200).send("Message processed successfully.");
+    } else {
+      // Pub/Sub-triggered
+      const event = reqOrEvent;
+      message = event.data
+        ? Buffer.from(event.data, "base64").toString()
+        : "{}";
+      attributes = event.attributes || {};
+    }
+
+    // Decode and process the message
+    const parsedMessage = JSON.parse(message);
+    console.log("Decoded message:", parsedMessage);
+    console.log("Message attributes:", attributes);
+    const { batchUuid, rows, customizations, originalname, email } = parsedMessage;
+
+    // Return for Pub/Sub
+    return;
+  } catch (error) {
+    console.error("Error processing Pub/Sub message:", error);
+
+    const parsedMessage = JSON.parse(message);
+    console.log("Decoded message:", parsedMessage);
+    console.log("Message attributes:", attributes);
+    
+    // Throw error for Pub/Sub
+    throw new Error("Failed to process Pub/Sub message");
+  }
+};
